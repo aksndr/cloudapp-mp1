12addaac7db8d10e856b75cb216c332ce0bbdde0
aksndr <aksndr@gmail.com>
2015-08-26 14:42:17 +0300

    finished

--- MP1.java
+++ MP1.java
@@ -1,5 +1,6 @@
-import java.io.File;
-import java.lang.reflect.Array;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
@@ -50,24 +51,72 @@ public class MP1 {
     }
 
     public String[] process() throws Exception {
-        String[] ret = new String[20];
-       
-        //TODO
 
-        return ret;
+        Map<String, Integer> map = new HashMap<String, Integer>();
+
+        Integer idx = 0;
+        List<String> stopWordsList = Arrays.asList(stopWordsArray);
+
+        List<Integer> indexes = Arrays.asList(getIndexes());
+        List<String> lines = Files.readAllLines(FileSystems.getDefault().getPath(this.inputFileName), StandardCharsets.UTF_8);
+        for (String line : lines) {
+            if (indexes.contains(idx)) {
+                //System.out.println(idx + " : " + line);
+                StringTokenizer st = new StringTokenizer(line, delimiters);
+                while (st.hasMoreElements()) {
+                    String word = st.nextToken();
+                    if (word.isEmpty() || stopWordsList.contains(word)) continue;
+                    if (map.containsKey(word)) {
+                        Integer count = map.get(word);
+                        map.put(word, ++count);
+                    } else {
+                        map.put(word, 1);
+                    }
+                }
+            }
+            idx++;
+        }
+        return getTop20RatedWordsArray(map);
+    }
+
+    private String[] getTop20RatedWordsArray(Map<String, Integer> map) {
+        ValueComparator bvc = new ValueComparator(map);
+        TreeMap<String, Integer> sorted_map = new TreeMap<String, Integer>(bvc);
+        sorted_map.putAll(map);
+
+        List<String> list = Collections.list(Collections.enumeration(sorted_map.keySet())).subList(0, 20);
+        return list.toArray(new String[20]);
     }
 
     public static void main(String[] args) throws Exception {
-        if (args.length < 1){
+        if (args.length < 1) {
             System.out.println("MP1 <User ID>");
-        }
-        else {
+        } else {
             String userName = args[0];
             String inputFileName = "./input.txt";
             MP1 mp = new MP1(userName, inputFileName);
             String[] topItems = mp.process();
-            for (String item: topItems){
+            for (String item : topItems) {
                 System.out.println(item);
+            }
+        }
+    }
+
+    class ValueComparator implements Comparator<String> {
+
+        Map<String, Integer> map;
+
+        public ValueComparator(Map<String, Integer> base) {
+            this.map = base;
+        }
+
+        public int compare(String a, String b) {
+            Integer ai = map.get(a);
+            Integer bi = map.get(b);
+            if (ai >= bi) {
+                return -1;
+            } else {
+                return 1;
             }
         }
     }
